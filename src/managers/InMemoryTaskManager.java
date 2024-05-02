545669cad26dda9415ebad614b5a19906554e107
Mandarinka <dmitrypar5hin@yandex.com>
2024-05-02 21:32:06 +0300

    пробелы!!!

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 public class InMemoryTaskManager implements TaskManager {
     int id = 100;
 
@@ -14,7 +15,7 @@ public class InMemoryTaskManager implements TaskManager {
     private final Map<Integer, Epic> epicMap = new HashMap<>();
     private final Map<Integer, Subtask> subtaskMap = new HashMap<>();
     private final List<Task> historyOfView = new ArrayList<>();
-    private final int SIZE = 10;
+    //private final int SIZE = 10;
     private HistoryManager historyManager;
 
     public InMemoryTaskManager(HistoryManager historyManager) {
@@ -29,8 +30,8 @@ public class InMemoryTaskManager implements TaskManager {
     public void createTask(Task task) {
        task.setId(counterId());
         taskMap.put(id, task);
-
     }
+
     @Override
     public void updateTask(Task task) {
         taskMap.put(task.getId(), task);
@@ -44,8 +45,8 @@ public class InMemoryTaskManager implements TaskManager {
 
         }
         return null;
-
     }
+
     public List<Task> getAllTask() {
        taskMap.get(id);
      return historyOfView;
@@ -65,28 +66,16 @@ public class InMemoryTaskManager implements TaskManager {
     public void createEpic(Epic epic) {
         //epic.setId(counterId());
         epicMap.put(epic.setId(counterId()), epic);
-
     }
 
     @Override
     public void updateEpic(Epic epic) {
         epicMap.put(epic.getId(), epic);
-
-
     }
 
     @Override
     public Task gettingEpic(int id) {
-        if (epicMap.containsKey(id)) {
-            if (historyOfView.size() <= SIZE)  {
-                historyOfView.add(epicMap.get(id));
-            } else {
-                historyOfView.remove(0);
-                historyOfView.add(epicMap.get(id));
-            }
-            return epicMap.get(id);
-        }
-        return null;
+        return epicMap.get(id);
     }
 
     @Override
@@ -98,6 +87,7 @@ public class InMemoryTaskManager implements TaskManager {
     public void deleteAllEpic() {
         epicMap.clear();
     }
+
     @Override
     public void checkEpic(int epicId){
         Epic thisEpic = epicMap.get(epicId);
@@ -109,7 +99,9 @@ public class InMemoryTaskManager implements TaskManager {
             int checkDone = 0;
 
             for (int id : subtasksIdInThisEpic) {
+
                 Subtask thisSubtask = subtaskMap.get(id);
+
                 if (thisSubtask.getStatus() == StatusTask.IN_PROGRESS) {
                     thisEpic.setStatus(StatusTask.IN_PROGRESS);
                     return;
@@ -155,23 +147,17 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public Task gettingSubtask(int id) {
-        if (subtaskMap.containsKey(id)) {
-            if (historyOfView.size() <= SIZE) {
-                historyOfView.add(subtaskMap.get(id));
-            } else {
-                historyOfView.remove(0);
-                historyOfView.add(subtaskMap.get(id));
-            }
+
             return subtaskMap.get(id);
-        }
-        return null;
     }
+
     public ArrayList<Subtask> getSubtasksFromEpic(int epicId) {
         ArrayList<Subtask> subtasksInEpic = new ArrayList();
         Epic thisEpic = epicMap.get(epicId);
         ArrayList<Integer> subtasksIdInEpic = thisEpic.getSubTaskIds();
 
         for (int subtaskId : subtasksIdInEpic) {
+
             subtasksInEpic.add(subtaskMap.get(subtaskId));
         }
 
